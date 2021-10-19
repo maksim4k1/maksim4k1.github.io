186b985480d9f7d609156814cd3ff3f3df91821c
Maksim Bazhenov <maksim4k1@gmail.com>
2021-10-19 21:09:55 +0600

    edited portfolio initialState and added list for projects with API

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,18 +3,38 @@ import Section from "./UI/Section";
 import Title from "../components/UI/Title";
 import { connect } from "react-redux";
 import List from "./UI/List";
+import AppLink from "./UI/AppLink";
+import { gap } from "../styles/mixins";
+import styled from "styled-components";
 
-function Portfolio ({portfolio}) {
+const Content = styled.div`
+  margin: 30px 0 0 0;
+  display: flex;
+  flex-flow: column;
+  ${gap("10px")}
+  color: var(--color-txt-gray);
+  font-size: 14px;
+  font-weight: 500;
+  text-align: center;
+`;
+
+function Portfolio ({projects, projectsWithApi}) {
   return(
     <Section id="portfolio">
       <Title>Портфолио</Title>
-      <List list={portfolio}/>
+      <List list={projects}/>
+      <Content>
+        <p>Для корректной работы следующих приложений требуется скачать локальный API по этой ссылке: <AppLink href="https://github.com/an4morph/academy-fake-api" target="_blank" rel="noreferrer">https://github.com/an4morph/academy-fake-api</AppLink>.</p>
+        <p>Инструкция по скачиванию и запуску находится по этой же <AppLink href="https://github.com/an4morph/academy-fake-api" target="_blank" rel="noreferrer">ссылке</AppLink>.</p>
+      </Content>
+      <List list={projectsWithApi}/>
     </Section>
   );
 }
 
 const stateToProps = (state) => ({
-  portfolio: state.portfolio
+  projects: state.portfolio.projects,
+  projectsWithApi: state.portfolio.projectsWithApi,
 });
 
 export default connect(stateToProps)(Portfolio);
--- src/redux/reducers/portfolioReducer.js
+++ src/redux/reducers/portfolioReducer.js
@@ -7,18 +7,26 @@ import game from "../../assets/webp/Game.webp";
 import twitter from "../../assets/webp/Twitter.webp";
 import lucasmontana from "../../assets/webp/LucasMontana.webp";
 import translight from "../../assets/webp/TransLight.webp";
+import ubereats from "../../assets/webp/UberEats.webp";
+import sweetbakery from "../../assets/webp/SweetBakery.webp";
 
-const initialState = [
-  {id: 0, name: 'Мой сайт портфолио', image: `${portfolio}`, link: "/"},
-  {id: 1, name: 'Одностраничный сайт автозапчастей "Авто Дом"', image: `${avtodom}`, link: "https://maksim4k1.github.io/avtodom-landing-page.kg/"},
-  {id: 2, name: 'Веб-приложение "Таймер"', image: `${timer}`, link: "https://maksim4k1.github.io/Timer/"},
-  {id: 3, name: 'Веб-приложение "Список задач"', image: `${todolist}`, link: "https://maksim4k1.github.io/todolist/"},
-  {id: 4, name: 'Веб-приложение "Загрузчик фотографий"', image: `${uploadimages}`, link: "https://maksim4k1.github.io/upload-images/"},
-  {id: 5, name: 'Игра "Крестики-Нолики"', image: `${game}`, link: "https://maksim4k1.github.io/noughts-and-crosses/"},
-  {id: 6, name: 'Веб-приложение Twitter', image: `${twitter}`, link: "https://maksim4k1.github.io/twitter/"},
-  {id: 7, name: 'Сайт портфолио "Lucas Montana"', image: `${lucasmontana}`, link: "https://maksim4k1.github.io/Lucas-Montana/"},
-  {id: 8, name: 'Одностраничный сайт "TRANS LIGHT"', image: `${translight}`, link: "https://maksim4k1.github.io/TRANS-LIGHT/"},
-];
+const initialState = {
+  projects: [
+    {id: 0, name: 'Мой сайт портфолио', image: `${portfolio}`, link: "/"},
+    {id: 1, name: 'Одностраничный сайт автозапчастей "Авто Дом"', image: `${avtodom}`, link: "https://maksim4k1.github.io/avtodom-landing-page.kg/"},
+    {id: 2, name: 'Веб-приложение "Таймер"', image: `${timer}`, link: "https://maksim4k1.github.io/Timer/"},
+    {id: 3, name: 'Веб-приложение "Список задач"', image: `${todolist}`, link: "https://maksim4k1.github.io/todolist/"},
+    {id: 4, name: 'Веб-приложение "Загрузчик фотографий"', image: `${uploadimages}`, link: "https://maksim4k1.github.io/upload-images/"},
+    {id: 5, name: 'Игра "Крестики-Нолики"', image: `${game}`, link: "https://maksim4k1.github.io/noughts-and-crosses/"},
+    {id: 6, name: 'Веб-приложение Twitter', image: `${twitter}`, link: "https://maksim4k1.github.io/twitter/"},
+    {id: 7, name: 'Сайт портфолио "Lucas Montana"', image: `${lucasmontana}`, link: "https://maksim4k1.github.io/Lucas-Montana/"},
+    {id: 8, name: 'Одностраничный сайт "TRANS LIGHT"', image: `${translight}`, link: "https://maksim4k1.github.io/TRANS-LIGHT/"},
+  ],
+  projectsWithApi: [
+    {id: 0, name: 'Приложение для просмотра меню в ресторанах "Uber Eats"', image: `${ubereats}`, link: "https://maksim4k1.github.io/UberEats/"},
+    {id: 1, name: 'Мини интернет-магазин "Sweet Bakery"', image: `${sweetbakery}`, link: "https://maksim4k1.github.io/Sweet-Bakery/"},
+  ],
+};
 
 const portfolioReducer = (state=initialState, {type}) => {
   switch(type){

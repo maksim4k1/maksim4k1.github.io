6938eb87979fd73cb632b4418bc157fe28285af5
Maksim Bazhenov <maksim4k1@gmail.com>
2021-10-20 17:39:14 +0600

    created ScrollToTop component and added him to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './App';
-import { HashRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import store from './redux';
+import App from './App';
+import ScrollToTop from './utils/ScrollToTop';
 
 ReactDOM.render(
   <Provider store={store}>
     <HashRouter>
       <React.StrictMode>
+        <ScrollToTop/>
         <App />
       </React.StrictMode>
     </HashRouter>

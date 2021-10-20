b4e2ac0303640772e0a78dd51450f3e573a46547
Maksim Bazhenov <maksim4k1@gmail.com>
2021-10-20 22:30:50 +0600

    created Skills section component and added him to About page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import About from "../components/About";
+import Skills from "../components/Skills";
 
 function AboutPage () {
   return(
     <main className="content">
       <About/>
+      <Skills/>
     </main>
   );
 }

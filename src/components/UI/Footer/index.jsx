7958eec7f709f2f7a92ce7a056eacd2e4bc8bb1f
Maksim Bazhenov <maksim4k1@gmail.com>
2021-10-20 16:45:19 +0600

    added navigation for Footer

--- src/components/UI/Footer/index.jsx
+++ src/components/UI/Footer/index.jsx
@@ -6,6 +6,7 @@ import PhoneIcon from "../../../assets/icons/PhoneIcon";
 import TelegramIcon from "../../../assets/icons/TelegramIcon";
 import WhatsappIcon from "../../../assets/icons/WhatsappIcon";
 import { gap } from "../../../styles/mixins";
+import AppLink from "../AppLink";
 import Section from "../Section";
 
 const FooterElement = styled.footer`
@@ -40,11 +41,36 @@ const Content = styled.p`
   font-weight: 500;
   text-align: center;
 `;
+const Navigation = styled.nav`
+  display: flex;
+  flex-flow: wrap;
+  justify-content: center;
+  ${gap("30px")}
+  &>a{
+    color: var(--color-white);
+  }
+  @media screen and (max-width: 600px){
+    flex-flow: column;
+    ${gap("15px")}
+    font-size: 14px;
+    &>a{
+      margin: 0 auto;
+    }
+    order: 1
+  }
+`;
 
 function Footer () {
   return(
     <FooterElement>
       <Section id="contacts">
+        <Navigation>
+          <AppLink type="navlink" to="/">Главная</AppLink>
+          <AppLink type="navlink" to="/about">Обо мне</AppLink>
+          <AppLink type="navlink" to="/certificates">Сертификаты</AppLink>
+          <AppLink type="navlink" to="/portfolio">Портфолио</AppLink>
+          <AppLink type="navlink" to="/contacts">Контакты</AppLink>
+        </Navigation>
         <Title>Контакты</Title>
         <List>
           <li><a target="_blank" rel="noreferrer" href="https://t.me/maksim4k1"><TelegramIcon/></a></li>
